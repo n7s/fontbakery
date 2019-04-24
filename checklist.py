7ce6c8f94816423c2eba2b192bcaa8dc27850dcc
Nicolas Spalinger <nicolas.spalinger@sil.org>
2019-04-24 10:19:10 +0200

    Updated to new module names and other fixes from Felipe.

--- checklist.py
+++ checklist.py
@@ -1,26 +1,29 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
+# script to export fontbakery checks for review
+# adapted from https://github.com/googlefonts/fontbakery/issues/2039
 
 import pkgutil
 
-from fontbakery import specifications
+from fontbakery import profiles
 from fontbakery.callable import FontBakeryCheck
 
 checks = []
 
-for importer, modname, ispkg in pkgutil.iter_modules(specifications.__path__):
-    exec('from fontbakery.specifications import ' + modname)
-    exec('module = ' + modname)
-    for attr_name in dir(module):
-        attr = getattr(module, attr_name)
-        if isinstance(attr, FontBakeryCheck):
-            checks.append((attr.__name__, attr.__doc__))
+for importer, modname, ispkg in pkgutil.iter_modules(profiles.__path__):
+  exec('from fontbakery.profiles import ' + modname)
+  exec('module = ' + modname)
+  for attr_name in dir(module):
+    attr = getattr(module, attr_name)
+    if isinstance(attr, FontBakeryCheck):
+      try:
+        rationale = f"\n## rationale:\n\t{attr.rationale}\n"
+      except:
+        rationale = ""
+      checks.append((attr.id, attr.__doc__, rationale))
 
 checks.sort()
 
-for name, doc in checks:
-    print('# ' + name)
-    print()
-    print(doc.strip())
-    print()
+for check_id, doc, rationale in checks:
+  print(f'# {check_id}\n{doc.strip()}{rationale}\n')
 
 
